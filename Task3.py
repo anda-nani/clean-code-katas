e094dca488a7f58ad6d2a48fe6cad3326a46d884
anda-nani <andananiandy@gmail.com>
2025-09-09 10:15:41 +0200

    Completed Task 3

---
+++ Task3.py
@@ -0,0 +1,13 @@
+def is_sixty_five(num1, num2) -> bool :
+
+    if num1 == 65 or num2 == 65 or num1 + num2 == 65:
+
+        return True
+    
+    else:
+
+        return False
+    
+print(is_sixty_five(65, 5))
+    
+

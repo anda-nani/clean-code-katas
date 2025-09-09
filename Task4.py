01d32560bb3c31b604d9e91d050f4e45a6cc5dbe
anda-nani <andananiandy@gmail.com>
2025-09-09 10:22:15 +0200

    Completed Task4

---
+++ Task4.py
@@ -0,0 +1,6 @@
+def square(num: int) -> None:
+    for _ in range(num):        #for the number of rows (value of argument)
+        print('# ' * num)       #printing a multiplied string (columns)
+
+square(2) 
+square(4)
